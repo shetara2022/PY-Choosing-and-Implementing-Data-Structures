4b9c066b89c6ae906e4ddb35a3cb8c294301ec77
shetara2022 <smithshetarall@gmail.com>
2022-12-11 15:39:21 -0500

    completed problem 3 and bonus

--- arrays/3.py
+++ arrays/3.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+#Use a single operation (no loops) to multiply every element of an array by 3
+a = np.array([5,3,4])
+
+product = a * 3
+
+print(product)
--- bonus_linked_list/linked_list.py
+++ bonus_linked_list/linked_list.py
@@ -1,1 +1,17 @@
 # Write code for implementing a linked list below
+
+class Node:
+    def __init__(self, data, reference=None):
+        self.data = data
+        self.reference = reference 
+
+class Linked_List:
+    def __init__(self, head = None):
+        self.head = head
+
+
+list1 = Linked_List()
+list1.self.head = Node("January")
+lst2 = Node("February")
+
+list1.head.reference = lst2
